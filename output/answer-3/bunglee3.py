62a7807b3b800e7f542f771406410f22ae737bfb
sowmyakattamuri61 <65445823+sowmyakattamuri61@users.noreply.github.com>
2021-10-02 13:05:20 +0530

    Update bunglee3.py

--- output/answer-3/bunglee3.py
+++ output/answer-3/bunglee3.py
@@ -5,5 +5,7 @@ cwd = os.getcwd()
 print(cwd)
 
 df = pd.read_csv(cwd + '/input/question-3/main.csv')
-result = df.groupby('occupation').agg({'age': ['min', 'max']})
+df = df[['Team', 'Yellow Cards', 'Red Cards']]
+result = df.sort_values('Yellow Cards', ascending=False).sort_values('Red Cards', ascending=False)
+
 result.to_csv(cwd + '/output/answer-3/main.csv')
